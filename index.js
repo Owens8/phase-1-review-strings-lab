9af1e60e6e2593524d7c134930160f2078ff1c76
Corey Tilson <owensdaniel8@yahoo.com>
2022-04-13 20:39:55 +0100

    commit tree 57821d068a1130fdb538739d9a77e2b1537ffb5a

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "cock"
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, COCK!`
+let shortGreeting = "Welcome, c!"
